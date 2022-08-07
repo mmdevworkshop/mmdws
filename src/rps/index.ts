49f7b67dcc688365c7ce5ffcb79caece8f159295
myndzi <mmdws@proton.me>
2022-08-06 20:02:25 -0700

    Add typings

--- src/rps/index.ts
+++ src/rps/index.ts
@@ -1,17 +1,25 @@
 import { basename, relative } from 'node:path';
 
-function pickOne(arr) {
+enum RPS {
+    ROCK = "rock",
+    PAPER = "paper",
+    SCISSORS = "scissors"
+}
+
+function pickOne(arr: RPS[]): RPS {
     const choice = Math.floor(Math.random() * arr.length);
     return arr[choice];
 }
 
-const beatenBy = {
-    "rock": "paper",
-    "paper": "scissors",
-    "scissors": "rock",
+const beatenBy: {
+    [K in RPS]: RPS
+} = {
+    [RPS.ROCK]: RPS.PAPER,
+    [RPS.PAPER]: RPS.SCISSORS,
+    [RPS.SCISSORS]: RPS.ROCK,
 };
 
-function getResult(userChoice, computerChoice) {
+function getResult(userChoice: RPS, computerChoice: RPS): string {
     if (userChoice === computerChoice) {
         return "It's a tie!";
     }
@@ -26,15 +34,22 @@ function getResult(userChoice, computerChoice) {
     throw new Error("Should never get here");
 }
 
-function cleanup(str) {
-    if (typeof str === "string") {
-        return str.trim().toLowerCase();
+function cleanup(str: string): RPS | null {
+    if (typeof str !== "string") {
+        return null;
     }
-    return null;
+
+    const clean: string = str.trim().toLowerCase();
+    if (!Object.hasOwnProperty.call(beatenBy, clean)) {
+        return null;
+    }
+    
+    // here, our input must be a string -- it also must be a valid choice
+    return clean as RPS;
 }
 
 function main() {
-    const choices = Object.keys(beatenBy);
+    const choices = Object.keys(beatenBy) as RPS[];
 
     const userChoice = cleanup(process.argv[2]);
     if (Object.hasOwnProperty.call(beatenBy, userChoice)) {
